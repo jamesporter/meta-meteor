6733ba295f23d8fb21b4d478c98b1ad3a845be5b
Harry Moss <harryjohnmoss@gmail.com>
2015-10-10 16:18:14 +0100

    Added collections, published to collections (if logged in), subscribed to collections

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,20 +2,20 @@ Meteor.publish("topics", function () {
     if (this.userId) {
         return Topics.find()
     } else {
-        return {}
+        this.ready()
     }
 });
 Meteor.publish("questions", function () {
     if (this.userId) {
         return Questions.find()
     } else {
-        return {}
+        this.ready()
     }
 });
 Meteor.publish("responses", function () {
     if (this.userId) {
         return Responses.find()
     } else {
-        return {}
+        this.ready()
     }
 });
