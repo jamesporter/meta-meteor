4983ecc5b3dec408f05b9427956bd95c058112ef
James Porter <james@amimetic.co.uk>
2015-10-10 20:40:14 +0100

    Hide manage options if not topic owner

--- client/components/question.jsx
+++ client/components/question.jsx
@@ -9,7 +9,9 @@ Question = React.createClass({
     propTypes: {
         question: React.PropTypes.object.isRequired
     },
-
+    showOwnerOptions(){
+        return this.props.topic.ownerId == this.props.user._id;
+    },
     renderOptions(){
         return this.props.question.options.map( opt => {
             return(
@@ -23,11 +25,14 @@ Question = React.createClass({
             <ListGroupItem>
                 <h2>{this.props.question.questionText}</h2>
 
+                { this.showOwnerOptions()?
                 <ButtonToolbar>
                     <Button>Mark</Button>
                     <Button href={"/topic/" + this.props.topic._id + "/question/" + this.props.question._id} >Edit</Button>
                     <Button>Delete</Button>
                 </ButtonToolbar>
+                    :
+                    ""}
 
                 <p>Please choose an option</p>
 
